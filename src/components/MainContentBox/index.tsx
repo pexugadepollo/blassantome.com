2a7a097e58a658c8337ff5a269c12221f019a002
pexugadepollo <bso080601@gmail.com>
2020-05-05 20:30:57 +0200

    Create main page

---
+++ src/components/MainContentBox/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import * as SC from './styles'
+
+const MainContentBox:FC = () => {
+    return(
+    <SC.Box>
+
+    </SC.Box>)
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,26 @@
-import React, {FC} from 'react';
+import React, { FC } from "react";
+import * as SC from "./styles";
 
-const Main:FC = () => {
-    return(<div></div>)
-}
+const Main: FC = () => {
+  return (
+      <div>
+    <SC.Background/>
+    <SC.MainContainer>
+        <SC.Box>
+            {/* <SC.AnimatedText>
+                <p>Hola</p>
+                <p>Soy Blas</p>
+                <p>Un mesaje</p>
+                <p>Otro mensaje</p>
+                <p>Otro mas</p>
+            </SC.AnimatedText> */}
+            <SC.BottomButtomBar>
+                
+            </SC.BottomButtomBar>
+        </SC.Box>
+    </SC.MainContainer>
+    </div>
+  );
+};
 
 export default Main;
