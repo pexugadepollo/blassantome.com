59d591d9b6378dc717caf4bdc599ac16c494f8ff
pexugadepollo <bso080601@gmail.com>
2020-05-09 15:03:57 +0200

    Create Spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import * as SC from "./styles";
+
+const Spinner: FC = () => {
+  return (
+    <SC.Spinner>
+      <div></div>
+      <div></div>
+    </SC.Spinner>
+  );
+};
+
+export default Spinner;
