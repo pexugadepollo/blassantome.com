07a7b8a8de863b84517c1f17fdda77315d615aae
pexugadepollo <bso080601@gmail.com>
2020-05-10 14:32:49 +0200

    Add functionality to tab Projects

--- src/pages/Main/tabs/Projects/index.tsx
+++ src/pages/Main/tabs/Projects/index.tsx
@@ -1,51 +1,12 @@
-import React, { FC, useEffect, useState } from "react";
-import { GraphQLClient } from "graphql-request";
+import React, { FC } from "react";
+import GithubRepos from '../../../../components/GithubRepos'
 
-const GitHubApi = new GraphQLClient('https://api.github.com/graphql', {
-        headers: {
-          Authorization: 'Bearer f630115b3a23bc5409cbc918495d8d950ccf4980',
-        },
-      });
-      const query = `
-      {
-        viewer {
-          repositories(first: 100, affiliations: [OWNER, COLLABORATOR, ORGANIZATION_MEMBER], privacy: PUBLIC) {
-            nodes{
-                name
-                  owner {
-                    login
-                  }          
-                }    
-              }
-           }
-      }
-      `;
 
 const Projects: FC = () => {
-  const [githubRepos, setGithub] = useState({
-    viewer: {
-      repositories: {
-        nodes: [
-          {
-            name: "",
-            owner: {
-              login: "",
-            },
-          },
-        ],
-      },
-    },
-  });
-  useEffect(()=>{
-    GitHubApi.request(query).then(data => setGithub(data))
-  },[])
-  const generateProjects = githubRepos.viewer.repositories.nodes.map((repo,index)=>{
-    return <li key={index}>{repo.name}</li>
-  });
+
+
   return (
-    <ul>
-      {generateProjects}
-    </ul>
+    <GithubRepos></GithubRepos>
   );
 };
 
