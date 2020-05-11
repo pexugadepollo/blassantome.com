fbce6d4b2dc5c515fcf9a794ce0704724dd922aa
pexugadepollo <bso080601@gmail.com>
2020-05-11 11:30:42 +0200

    Fix CircleButtons on acces directly to tabs

--- src/pages/Main/tabs/Contact/index.tsx
+++ src/pages/Main/tabs/Contact/index.tsx
@@ -1,6 +1,11 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useTab } from "../../../../hooks/useTab";
 
 const Contact: FC = () => {
+  const { setTab } = useTab();
+  useEffect(()=>{
+    setTab(3)
+  }, [setTab])
   return <div>contacto</div>;
 };
 
--- src/pages/Main/tabs/Projects/index.tsx
+++ src/pages/Main/tabs/Projects/index.tsx
@@ -1,13 +1,13 @@
-import React, { FC } from "react";
-import GithubRepos from '../../../../components/GithubRepos'
-
+import React, { FC, useEffect } from "react";
+import GithubRepos from "../../../../components/GithubRepos";
+import { useTab } from "../../../../hooks/useTab";
 
 const Projects: FC = () => {
-
-
-  return (
-    <GithubRepos></GithubRepos>
-  );
+  const { setTab } = useTab();
+  useEffect(()=>{
+    setTab(1)
+  }, [setTab])
+  return <GithubRepos></GithubRepos>;
 };
 
 export default Projects;
