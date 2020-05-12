b4a3291dce303cc8e38e96fe8e0bf186178422c1
pexugadepollo <bso080601@gmail.com>
2020-05-12 23:23:27 +0200

    Update Home

--- src/pages/Main/tabs/Home/index.tsx
+++ src/pages/Main/tabs/Home/index.tsx
@@ -1,22 +1,12 @@
 import React, { FC, useEffect } from "react";
-import { Spinner } from "../../../../components/Spinner/styles";
-import * as SC from './style'
 import { useTab } from "../../../../hooks/useTab";
 
 const Home: FC = () => {
   const { setTab } = useTab();
-  useEffect(()=>{
-    setTab(2)
-  }, [setTab])
-  return (
-    <SC.AnimatedText>
-      <p>Hola</p>
-      <p>Soy Blas</p>
-      <p>Un mesaje</p>
-      <p>Otro mensaje</p>
-      <p>Otro mas</p>
-    </SC.AnimatedText>
-  );
+  useEffect(() => {
+    setTab(2);
+  }, [setTab]);
+  return <div></div>;
 };
 console.log(Home);
 export default Home;
