b9205f295864c856af64340ffa05d07919afefda
pexugadepollo <53176662+pexugadepollo@users.noreply.github.com>
2020-05-13 21:27:12 +0200

    Add validation

--- src/pages/Main/tabs/Contact/index.tsx
+++ src/pages/Main/tabs/Contact/index.tsx
@@ -11,12 +11,11 @@ const Contact: FC = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [formErrors, setFormErrors] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    body: ""
-  })
-  const [response, setResponse] = useState({});
+    name: " ",
+    email: " ",
+    subject: " ",
+    body: " ",
+  });
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -30,7 +29,6 @@ const Contact: FC = () => {
         "https://api.blassantome.com/mail/send",
         formData
       );
-      setResponse(response);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -47,38 +45,57 @@ const Contact: FC = () => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
   const handleOnClick = () => {
-    sendMail();
+    const validFormErrors = {
+      name: "",
+      email: "",
+      subject: "",
+      body: "",
+    };
+    if (formErrors === validFormErrors) {
+      sendMail();
+    }
   };
   const handleBlur = (
     e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     switch (e.target.name) {
       case "name":
-        if(e.target.value.length<2){
-          setFormErrors({...formErrors, name: "El nombre debe contener al menos 2 caracteres"})
-        }else{
-          setFormErrors({...formErrors, name: ""})
+        if (e.target.value.length < 2) {
+          setFormErrors({
+            ...formErrors,
+            name: "El nombre debe contener al menos 2 caracteres",
+          });
+        } else {
+          setFormErrors({ ...formErrors, name: "" });
         }
         break;
       case "email":
-        if(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value)){
-          setFormErrors({...formErrors, email: ""})
-        }else{
-          setFormErrors({...formErrors, email: "El formato no es correcto"})
+        if (
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value)
+        ) {
+          setFormErrors({ ...formErrors, email: "" });
+        } else {
+          setFormErrors({ ...formErrors, email: "El formato no es correcto" });
         }
         break;
       case "subject":
-        if(e.target.value.length<2){
-          setFormErrors({...formErrors, subject: "El nombre debe contener al menos 2 caracteres"})
-        }else{
-          setFormErrors({...formErrors, subject: ""})
+        if (e.target.value.length < 2) {
+          setFormErrors({
+            ...formErrors,
+            subject: "El nombre debe contener al menos 2 caracteres",
+          });
+        } else {
+          setFormErrors({ ...formErrors, subject: "" });
         }
         break;
       case "body":
-        if(e.target.value.length<15){
-          setFormErrors({...formErrors, body: "El mensaje no puede ser tan corto"})
-        }else{
-          setFormErrors({...formErrors, body: ""})
+        if (e.target.value.length < 15) {
+          setFormErrors({
+            ...formErrors,
+            body: "El mensaje no puede ser tan corto",
+          });
+        } else {
+          setFormErrors({ ...formErrors, body: "" });
         }
         break;
       default:
