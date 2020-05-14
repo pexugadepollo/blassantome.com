d8d8c83619967e143e2db7000da476c53dfbb2b5
pexugadepollo <53176662+pexugadepollo@users.noreply.github.com>
2020-05-14 21:14:31 +0200

    Fixed Spinner

--- src/components/Spinner/styles.tsx
+++ src/components/Spinner/styles.tsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+export const SpinnerContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 export const Spinner = styled.div`
   display: inline-block;
   position: relative;
